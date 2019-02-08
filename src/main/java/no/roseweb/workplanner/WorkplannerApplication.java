169175b720d037ab2f91a4a206e6bca0528701dd
Øystein Rose <oystein.rose@gmail.com>
2019-02-08 22:12:40 +0100

    Added spring boot application

---
+++ src/main/java/no/roseweb/workplanner/WorkplannerApplication.java
@@ -0,0 +1,14 @@
+package no.roseweb.workplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkplannerApplication.class, args);
+	}
+
+}
+
