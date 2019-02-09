31f09e9e7f24367b95a86b4c0675c85dbfde557b
Øystein Rose <oystein.rose@gmail.com>
2019-02-09 14:08:38 +0100

    Implemented user save and findByEmail

---
+++ src/main/java/no/roseweb/workplanner/models/rowmappers/UserRowMapper.java
@@ -0,0 +1,20 @@
+package no.roseweb.workplanner.models.rowmappers;
+
+import no.roseweb.workplanner.models.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet rs, int i) throws SQLException {
+        User user = new User();
+
+        user.setEmail(rs.getString("email"));
+        user.setFirstname(rs.getString("firstname"));
+        user.setLastname(rs.getString("lastname"));
+
+        return user;
+    }
+}
--- src/main/java/no/roseweb/workplanner/repository/UserRepositoryImpl.java
+++ src/main/java/no/roseweb/workplanner/repository/UserRepositoryImpl.java
@@ -1,21 +1,43 @@
 package no.roseweb.workplanner.repository;
 
 import no.roseweb.workplanner.models.User;
+import no.roseweb.workplanner.models.rowmappers.UserRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRepositoryImpl implements UserRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    public UserRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void save(User user) {
+        String sql = "insert into user (email, firstname, lastname, password) values (?, ?, ?, ?)";
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        jdbcTemplate.update(sql, user.getEmail(), user.getFirstname(), user.getLastname(), user.getPassword());
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        String sql = "select * from user where email = ?";
+
+        User user = (User)jdbcTemplate.queryForObject(
+                sql,
+                new Object[] { email },
+                new UserRowMapper()
+        );
+
+        return user;
     }
 }
