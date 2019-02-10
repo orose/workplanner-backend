a8fc987c4e1a90893eae74e6108fa89fddcd2086
Øystein Rose <oystein.rose@gmail.com>
2019-02-10 07:14:51 +0100

    Created team table

--- src/main/resources/db/migration/V1_1__create_organization_table.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists organization
-(
-    id int not null auto_increment primary key,
-    name varchar(255) not null,
-    email varchar(255),
-    organization_number varchar(255)
-) default CHARSET=UTF8MB4;
