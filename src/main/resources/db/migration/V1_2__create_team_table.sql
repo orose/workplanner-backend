6d972d83f1139bbcf8a4aa39ab9ed9d5bc786603
Øystein Rose <oystein.rose@gmail.com>
2019-02-15 21:26:55 +0100

    Removed duplicate migration file

--- src/main/resources/db/migration/V1_2__create_team_table.sql
+++ src/main/resources/db/migration/V1_2__create_team_table.sql
@@ -2,6 +2,6 @@ create table if not exists team
 (
     id int not null auto_increment primary key,
     name varchar(255) not null,
-    organization_id int,
+    organization_id int not null,
     constraint fk_organization__team foreign key (organization_id) references organization(id)
-) default CHARSET=UTF8MB4;
+);
--- src/main/resources/db/migration/V1_6__create_team_table.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists team
-(
-    id int not null auto_increment primary key,
-    name varchar(255) not null,
-    organization_id int not null,
-    constraint fk_organization__team foreign key (organization_id) references organization(id)
-);
