871a81fbb8d1e8b6824313ff7224507ccf883911
Øystein Rose <oystein.rose@gmail.com>
2019-02-27 11:52:18 +0100

    Added organization controller

---
+++ src/test/java/no/roseweb/workplanner/controllers/OrganizationControllerTest.java
@@ -0,0 +1,38 @@
+package no.roseweb.workplanner.controllers;
+
+import no.roseweb.workplanner.models.Organization;
+import no.roseweb.workplanner.repositories.OrganizationRepositoryImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrganizationControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockUser
+    public void getShouldReturnOrganization() throws Exception {
+        OrganizationRepositoryImpl organizationRepository = mock(OrganizationRepositoryImpl.class);
+        when(organizationRepository.findById(ArgumentMatchers.anyLong())).thenReturn(new Organization());
+
+        mvc.perform(get("/organization/1")
+            .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+}
