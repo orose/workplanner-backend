49265fe91a26b30d5503b751b738d1d297ce764e
Øystein Rose <oystein.rose@gmail.com>
2019-09-02 22:24:27 +0200

    Checkstyle fix - imports

--- src/main/java/no/roseweb/workplanner/controllers/WorkorderController.java
+++ src/main/java/no/roseweb/workplanner/controllers/WorkorderController.java
@@ -3,7 +3,13 @@ package no.roseweb.workplanner.controllers;
 import no.roseweb.workplanner.models.Workorder;
 import no.roseweb.workplanner.models.WorkorderListResponse;
 import no.roseweb.workplanner.repositories.WorkorderRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
